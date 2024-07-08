d6349284c0f284b702f2b19838f7ed5e9f70f139
syrongk <syrongk@gmail.com>
2024-07-08 10:25:10 +0900

    단어 뒤집기 - solution 1

--- src/main/java/org/algorism/lecture/string/four/SolutionOne.java
+++ src/main/java/org/algorism/lecture/string/four/SolutionOne.java
@@ -7,9 +7,14 @@ public class SolutionOne {
     private static void solution() {
         Scanner scanner = new Scanner(System.in);
         int count = scanner.nextInt();
+        /*
+        nextInt() -> 줄바꿈 문자가 입력 스트림에 남아 있다.
+        이 상태에서 nextLine()으로 다음 입력값을 받으려하면, 줄바꿈 문자를 인식해서 빈 문자열을 입력으로 받아들인다.
+         */
+
 
         for (int i = 0; i < count; i++) {
-            String word = scanner.nextLine();
+            String word = scanner.next();
 
             char[] chars = word.toCharArray();
             for (int j = chars.length - 1; j >= 0; j--) {
