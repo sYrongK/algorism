096a2d909cb719f3c3cd55821dcf2b10509f717b
syrongk <syrongk@gmail.com>
2024-07-11 11:30:49 +0900

    프로그매러스 lv.2 최소값 최대값 - solution 1

---
+++ src/main/java/org/algorism/programmers/lv2/one/SolutionOne.java
@@ -0,0 +1,29 @@
+package org.algorism.programmers.lv2.one;
+
+public class SolutionOne {
+
+    private static String solution(String s) {
+        String[] str = s.split(" ");
+
+        String minVal = str[0];
+        String maxVal = str[0];
+
+        for (String element : str) {
+            int num = Integer.parseInt(element);
+
+            if (num < Integer.parseInt(minVal)) {
+                minVal = element;
+            }
+            if (num > Integer.parseInt(maxVal)) {
+                maxVal = element;
+            }
+        }
+        return minVal + " " + maxVal;
+    }
+
+    public static void main(String[] args) {
+        String answer = solution("-1 -1");
+
+        System.out.println(answer);
+    }
+}
