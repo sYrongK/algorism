6d20d24b234fed2d3955b43ae510406623b17885
syrongk <syrongk@gmail.com>
2024-07-11 11:44:25 +0900

    프로그매러스 lv.2 최소값 최대값 - solution 3

---
+++ src/main/java/org/algorism/programmers/lv2/one/SolutionThree.java
@@ -0,0 +1,24 @@
+package org.algorism.programmers.lv2.one;
+
+import java.util.Arrays;
+
+//Arrays.sort 이용
+public class SolutionThree {
+
+    private static String solution(String s) {
+        String[] str = s.split(" ");
+
+        int[] arr = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(arr);//오름차순 정렬
+        return arr[0] + " " + arr[arr.length - 1];
+    }
+
+    public static void main(String[] args) {
+        String answer = solution("-1 -2 -3 -4");
+
+        System.out.println(answer);
+    }
+}
