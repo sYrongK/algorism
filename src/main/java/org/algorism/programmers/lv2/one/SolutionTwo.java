e559b62a6780f88d27f55a2369fb0e1ba7dbbbb9
syrongk <syrongk@gmail.com>
2024-07-11 11:37:26 +0900

    프로그매러스 lv.2 최소값 최대값 - solution 1

---
+++ src/main/java/org/algorism/programmers/lv2/one/SolutionTwo.java
@@ -0,0 +1,30 @@
+package org.algorism.programmers.lv2.one;
+
+//순수 알고리즘
+public class SolutionTwo {
+
+    private static String solution(String s) {
+        String[] str = s.split(" ");
+
+        int min, max, n;
+        min = max = Integer.parseInt(str[0]);
+
+        for (String element : str) {
+            n = Integer.parseInt(element);
+
+            if (n < min) {
+                min = n;
+            }
+            if (n > max) {
+                max = n;
+            }
+        }
+        return min + " " + max;
+    }
+
+    public static void main(String[] args) {
+        String answer = solution("1 6 5 3 7");
+
+        System.out.println(answer);
+    }
+}
