0849cff5745ab26a6a9c9ddea0b819de517341e5
syrongk <syrongk@gmail.com>
2024-07-12 12:12:39 +0900

    프로그매러스 lv.2 올바른 괄호  - 속도 측정

---
+++ src/main/java/org/algorism/programmers/lv2/two/Time.java
@@ -0,0 +1,24 @@
+package org.algorism.programmers.lv2.two;
+
+public class Time {
+
+    public static void main(String[] args) {
+        estimate(() -> SolutionOne.solution("((((((()))()()()()()(())))"));
+        estimate(() -> SolutionTwo.solution("((((((()))()()()()()(())))"));
+    }
+
+    private static void estimate(Runnable runnable) {
+        int iterations = 1000;
+        long totalDuration = 0;
+        for (int i = iterations; i > 0; i--) {
+            long startTime = System.nanoTime();
+            runnable.run();
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            totalDuration += duration;
+        }
+        long averageDuration = totalDuration / iterations;
+        System.out.println("평균 실행 시간: " + averageDuration + " 나노초");
+    }
+}
