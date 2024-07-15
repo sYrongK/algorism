ae9f9e0508536c84b655d4a7d20d9363529616da
syrongk <syrongk@gmail.com>
2024-07-15 20:47:34 +0900

    프로그래머스 lv.2 이진 변환 반복하기 - solution 1

--- src/main/java/org/algorism/programmers/lv2/easy/q5/SolutionOne.java
+++ src/main/java/org/algorism/programmers/lv2/easy/q5/SolutionOne.java
@@ -1,12 +1,26 @@
 package org.algorism.programmers.lv2.easy.q5;
 
+import java.util.Arrays;
+
 public class SolutionOne {
 
-    public static void solution() {
+    public static int[] solution(String s) {
+        int[] answer = new int[2];
 
+        int temp;
+
+        while (!s.equals("1")) {
+            answer[1] += s.length();
+            s = s.replaceAll("0", "");
+            temp = s.length();
+            s = Integer.toBinaryString(temp);//1111 2진법으로 변경
+            answer[0]++;
+            answer[1] -= temp;// 원래 length - 0 제거 length
+        }
+        return answer;
     }
-
     public static void main(String[] args) {
-
+        int[] solution = solution("0111010");
+        System.out.println(Arrays.toString(solution));
     }
 }
