f86b348983ec018756eae2df091bdda4384f13f8
syrongk <syrongk@gmail.com>
2024-09-16 16:28:01 +0900

    선택정렬 연습

--- src/main/java/org/algorism/lecture/sort/one/Solution.java
+++ src/main/java/org/algorism/lecture/sort/one/Solution.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class Solution {
 
-    public static int[] solution(int[] nums) {
+    public static int[] solution(int[] arr) {
         //이중 for문:
         //배열의 자리 순차대로 비교하면서 그 자리에 들어올 가장 작은 수를 구할 것. i loop
         //기준값 idx(i로 시작해서 작은 값의 idx로 바꿔준다.)을 두고
@@ -19,22 +19,22 @@ public class Solution {
         // 배열의 idx보다 작은 수가 없다면(배열의 idx가 제일 작다면) 배열의 i에 배열의 idx값 대입.
         // 이걸 loop 반복.
 
-        for (int i = 0; i < nums.length - 1; i++) {//마지막까지 돌 필요는 없음 앞에서 비교해서 바꾸니까.
+        for (int i = 0; i < arr.length - 1; i++) {//마지막까지 돌 필요는 없음 앞에서 비교해서 바꾸니까.
             int idx = i;// i번째 자리에 들어올 숫자의 index 찾기
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] < nums[idx]) idx = j;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[idx]) idx = j;
             }
             //i번째 숫자와 idx번째 숫자 swap
             System.out.println("i : " + i);
             System.out.println("idx : " + idx);
             if (idx != i) {
                 System.out.println("Swap >>>> ");
-                int tmp = nums[i];
-                nums[i] = nums[idx];
-                nums[idx] = tmp;
+                int tmp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = tmp;
             }
         }
-        return nums;
+        return arr;
     }
 
     public static void main(String[] args) {
