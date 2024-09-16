f6f3ce64cc2cb3907c4e5557cf95bb6219422d78
syrongk <syrongk@gmail.com>
2024-09-16 19:50:43 +0900

    재귀함수 연습

---
+++ src/main/java/org/algorism/lecture/recursion/Solution.java
@@ -0,0 +1,36 @@
+package org.algorism.lecture.recursion;
+
+/**
+ * 재귀함수 - stackFrame
+ */
+public class Solution {
+
+    public static void recurve1(int n) {
+        /*
+        1부터 n까지 출력
+         */
+        if (n == 0) return;//void type 에서 return 쓰면 메서드 종료 의미
+        else {
+            recurve1(n-1);
+            System.out.println(n);//제일 마지막 호출된 재귀부터 처리하니까 - 스택프레임 순으로
+        }
+    }
+
+    private static void recurve2(int n) {
+        /*
+        10진수 N 재귀함수 써서 2진수로 변환 출력
+        10진수 -> 2진수
+        n을 2로 나누어지지 않을 때까지 나눈 후 역순으로 나머지값들을 조합하면 2진수
+         */
+        if (n == 0) return;
+        else {
+            recurve2(n/2);
+            System.out.print(n%2);
+        }
+    }
+
+    public static void main(String[] args) {
+//        recurve1(10);
+        recurve2(11);
+    }
+}
