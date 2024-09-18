10b8d7003698b0783017878845bc0c602baf3f7a
syrongk <syrongk@gmail.com>
2024-09-18 16:55:41 +0900

    트리 깊이우선탐색

--- src/main/java/org/algorism/lecture/tree/DfsTwo.java
+++ src/main/java/org/algorism/lecture/tree/DfsTwo.java
@@ -11,31 +11,32 @@ import java.util.stream.IntStream;
 public class DfsTwo {
 
     public static void solution(int n) {
-        int[] arr = new int[n+1];
+        int[] arr = new int[n+1];//각 index 가 자연수(노드의 data)가 되는 것!
         traverse(arr, n, 1);
     }
 
     public static void traverse(int[] arr, int n, int L) {
-        //출력은 dfs 하면 되는데, 경우의 수를 어떻게 구하지?
-        //사용한다 & 사용 안 한다를 자식 노드로!!
-        //n까지 체크해야하니까 일단 n+1 레벨까진 내려가야 함!
-        //L == n + 1 이면 말단노드 끝. -> 여기서 출력 등 하고 싶은 일 해주기
+        /*
+        사용한다 & 사용 안 한다 경우의 수를 자식 노드로!!
 
+        깊이를 L.
+        n까지 사용한다 안한다 경우의 수 뻗어나가야 하니까 일단 n+1 레벨까진 내려가야 함!
+        -> L == n + 1 이면 말단노드 끝.
+        -> 말단노드에서 출력 등 하고 싶은 일 해주기
+         */
         if (L == n + 1) {
             StringBuilder sb = new StringBuilder();
-            IntStream.range(0, arr.length)
+            IntStream.range(1, arr.length)//인덱스 출력
                     .filter(i -> arr[i] == 1)//공집합 제외됨
-                    .forEach(sb::append);
+                    .forEach(i -> sb.append(i).append(" "));
             if (sb.length() > 0) System.out.println(sb.toString());
         }
         else {
             arr[L] = 1;//사용하는 경우
             traverse(arr, n, L+1);
-
+// -> 사용하는 경우 체크하고, 사용 안 하는 경우 체크!
             arr[L] = 0;//사용 안 하는 경우
             traverse(arr, n, L+1);
-
-            // -> 사용하는 경우 체크하고, 사용 안 하는 경우 체크!
         }
 
     }
